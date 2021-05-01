85510db40b5ee6a1678bf693e63d919d19fac01c
fanghaow <2357322866@qq.com>
2021-05-01 22:19:10 +0800

    send yellow leaves and white flowers to gui

--- Nano_project/txt_tf_class.py
+++ Nano_project/txt_tf_class.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # encoding:utf-8
-# This file is used to tf information that only support yellow leaves!
 import numpy as np
 import socket
 import sys
@@ -8,25 +7,27 @@ import os
 import time
 import shutil
 #RPi's IP
-SERVER_IP = "192.168.43.122"
+SERVER_IP = "192.168.43.207" # zyf : 192.168.43.122
 SERVER_PORT = 8888
 lsta = [str(20) for i in range(24)]
-green, yellow = 0, 0
+white, yellow = 0, 0
 yellow_lst = [str(20) for i in range(12)]
-green_lst = [str(20) for i in range(12)]
-order = 1
+white_lst = [str(20) for i in range(12)]
+order = 1 # iteration order
+# try to remove our last files
 try:
     shutil.rmtree('home/dlinano/tensorrtx-master/yolov5/build/results')
     os.mkdir('home/dlinano/tensorrtx-master/yolov5/build/results')
 except:
     pass
+
 class data_tf_gui():
     def __init__(self):
         pass
 
     def readfile(self):
         global order
-        while(True):
+        while(1):
             try:
                 f = open(r"/home/dlinano/tensorrtx-master/yolov5/build/results/" + str(order) +".jpg.txt", "r") 
                 time.sleep(0.5)
@@ -64,7 +65,7 @@ class data_tf_gui():
                 else:
                     str_list[i] = item[1:]
 
-            for j,str_num in enumerate(str_list):
+            for j, str_num in enumerate(str_list):
                 data[index][j] = int(str_num)
         #
         print('filename:',imgfile)
@@ -76,7 +77,7 @@ class data_tf_gui():
     def transport(self):
         global lsta
         str_lsta = ''
-        for index,item in enumerate(lsta):
+        for index, item in enumerate(lsta):
             str_lsta += str(item)
             if index <= 22:
                 str_lsta += '/'
@@ -99,7 +100,7 @@ class data_tf_gui():
         while True:
             try:
                 data = socket_tcp.recv(512)
-                if len(data)>0:
+                if len(data) > 0:
                     print("Received: %s" % data.decode())
                     #command=input()
                     command = str_lsta
@@ -111,38 +112,47 @@ class data_tf_gui():
                     #continue
             except Exception:
                 socket_tcp.close()
-                socket_tcp=None
+                socket_tcp = None
                 sys.exit(1)
 
 def main():
     global lsta
     tf = data_tf_gui()
     for i in range(12):
-        num, data, leaf_class  = tf.readfile()
+        num, data, leaf_class = tf.readfile()
         if num == 0:
             yellow_lst[i] = 0
+            white_lst[i] = 0
         else:
             print('%%%%%%',leaf_class)
+            yellow_lst[i] = 0
+            white_lst[i] = 0
             if '0' in leaf_class:
                 yellow_lst[i] = 0
             if '1' in leaf_class:
-                pass
+                white_lst[i] = 0
             for j, name in enumerate(leaf_class):
                 if name == '0':
                     yellow_lst[i] += 1
                 if name == '1':
-                    pass
-            print('#####',yellow_lst)
-            
+                    white_lst[i] += 1
+            print('#####', yellow_lst)
+            print('$$$$$', white_lst)
         strange_order = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
         for index, order1 in enumerate(strange_order):
-            # lsta[(order1 + 1) * 2 - 1] = green_lst[index]
+            lsta[(order1 + 1) * 2 - 1] = white_lst[index]
             lsta[(order1 + 1) * 2 - 2] = yellow_lst[index]
 
         print('send data:', lsta)
+        time.sleep(500)
         tf.transport()
-        os.remove("/home/dlinano/tensorrtx-master/yolov5/build/results/" + str(i+1) +".jpg.txt")
+        # rm img file we produced
+        try:
+            os.remove("/home/dlinano/tensorrtx-master/yolov5/build/results/" + str(i+1) +".jpg.txt")
+        except:
+            pass
+        finally:
+            print('I accomplish number ', i, 'iteration')
 
 if __name__ == '__main__':
     main()
-    print('success!')
