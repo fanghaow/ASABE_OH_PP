d9964fa15e264e89280121831563921f94bbb495
fanghaow <2357322866@qq.com>
2021-05-17 17:21:27 +0800

    Edit it into fanghaow's code style

--- Raspberry_project/Navigation.py
+++ Raspberry_project/Navigation.py
@@ -7,85 +7,45 @@ import numpy as np
 import shutil
 import os
 
-f = open('yours.txt', 'w')
-f.close()
-os.remove('yours.txt')
-f = open('yours.txt', 'w')
-f.close()
-
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(15, GPIO.IN)
+GPIO.setup(15, GPIO.IN) # Infrared sensor
 GPIO.setup(16, GPIO.OUT)
-GPIO.setup(20, GPIO.OUT)
+GPIO.setup(20, GPIO.OUT) # Motor
 GPIO.setup(21, GPIO.OUT)
-GPIO.setup(12, GPIO.OUT)
-GPIO.setup(26, GPIO.OUT)
+GPIO.setup(12, GPIO.OUT) # Servo for Turning
+GPIO.setup(26, GPIO.OUT) # Servo for Camera
 GPIO.setup(24, GPIO.OUT)
 
-def GoAhead(step):
+def GoAhead(step, delay=1e-4):
     for i in range(step):
-        #pwm1 = GPIO.PWM(20, 100000)
-        #pwm1.start(50)
         GPIO.output(20, GPIO.HIGH)
-        #GPIO.output(26, GPIO.HIGH)
-        time.sleep(0.0001)
-        #GPIO.output(26, GPIO.LOW)
-        #time.sleep(0.00005)
-        #GPIO.output(26, GPIO.HIGH)
+        time.sleep(delay)
         GPIO.output(20, GPIO.LOW)
-        #time.sleep(0.0002)
-        #GPIO.output(26, GPIO.LOW)
-        time.sleep(0.0001)
-        #time.sleep(0.00001)
-        #pwm1.stop()
+        time.sleep(delay)
 
-#cap = cv2.VideoCapture(0)
-#cap.set(3, 1920)
-#cap.set(4, 1080)
-#ret, img = cap.read()
-#Port = "/dev/ttyUSB0"
-#Port1 = "/dev/ttyUSB1"
-#baudRate = 9600
+def Turn(angle):
+    pwm = GPIO.PWM(12, 50)
+    pwm.start(8)
+    pwm.ChangeDutyCycle(angle / 18. + 3)
+    time.sleep(0.3)
 
-angle = 150
-pwm = GPIO.PWM(12, 50)
-pwm.start(8)
-pwm.ChangeDutyCycle(angle / 18. + 3)
-time.sleep(0.3)
 angle2 = 100
 pwm2 = GPIO.PWM(26, 50)
 pwm2.start(8)
 pwm2.ChangeDutyCycle(angle2 / 18. + 3)
 time.sleep(0.3)
-#pwm.stop()
 
-#try:
-    #ser = serial.Serial(Port, baudRate, timeout = 0)
-#except:
-    #ser = serial.Serial(Port1, baudRate, timeout = 0)
+def main():
+    for i in range(12):
+        GoAhead(100)
+        if (GPIO.input(15) == 0): # Infrared sensor changed!!
+            print('Detect No.'+str(i)+' plant!!!')
+            time.sleep(3000)
+            GoAhead(2000)
 
-order = 0
+    GPIO.cleanup()
+    sys.exit(0)
 
-while True:
-    GoAhead(100)
-    #str1 = ''
-    #str1 = ser.readline().decode()
-    if (GPIO.input(15) == 0):
-        #ret, img = cap.read()
-        #img = img[:,460:1460,:]
-        #img = cv2.subtract(img, np.ones(img.shape, dtype = np.uint8)* 20)
-        #cv2.imwrite('/home/pi/Desktop/standard_photos' + '/' + str(order) + '.jpg', img)
-        f = open('yours.txt', 'w')
-        f.write(str(order))
-        f.close()
-        print('Save OK')
-        #client('/home/pi/Desktop' + '/' + str(order) + '.jpg')
-        GoAhead(2000)
-        order += 1
-        if order > 11:
-            break
-
-
-GPIO.cleanup()
-sys.exit(0)
+if __name__ == "__main__":
+    main()
