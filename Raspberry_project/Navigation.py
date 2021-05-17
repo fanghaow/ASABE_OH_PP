9067543ac053cfc8d895c9c0c9d179ac172f2bce
fanghaow <2357322866@qq.com>
2021-05-17 17:50:27 +0800

    Edit Nav into OOP

--- Raspberry_project/Navigation.py
+++ Raspberry_project/Navigation.py
@@ -7,45 +7,51 @@ import numpy as np
 import shutil
 import os
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(15, GPIO.IN) # Infrared sensor
-GPIO.setup(16, GPIO.OUT)
-GPIO.setup(20, GPIO.OUT) # Motor
-GPIO.setup(21, GPIO.OUT)
-GPIO.setup(12, GPIO.OUT) # Servo for Turning
-GPIO.setup(26, GPIO.OUT) # Servo for Camera
-GPIO.setup(24, GPIO.OUT)
+class Navi():
+    def __init__(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setup(15, GPIO.IN) # Infrared sensor
+        GPIO.setup(16, GPIO.OUT)
+        GPIO.setup(20, GPIO.OUT) # Motor
+        GPIO.setup(21, GPIO.OUT)
+        GPIO.setup(12, GPIO.OUT) # Servo for Turning
+        GPIO.setup(26, GPIO.OUT) # Servo for Camera
+        GPIO.setup(24, GPIO.OUT)
 
-def GoAhead(step, delay=1e-4):
-    for i in range(step):
-        GPIO.output(20, GPIO.HIGH)
-        time.sleep(delay)
-        GPIO.output(20, GPIO.LOW)
-        time.sleep(delay)
+    def GoAhead(self, step, delay=1e-4):
+        for i in range(step):
+            GPIO.output(20, GPIO.HIGH)
+            time.sleep(delay)
+            GPIO.output(20, GPIO.LOW)
+            time.sleep(delay)
 
-def Turn(angle):
-    pwm = GPIO.PWM(12, 50)
-    pwm.start(8)
-    pwm.ChangeDutyCycle(angle / 18. + 3)
-    time.sleep(0.3)
+    def Turn(self, angle):
+        pwm = GPIO.PWM(12, 50)
+        pwm.start(8)
+        pwm.ChangeDutyCycle(angle / 18. + 3)
+        time.sleep(0.3)
 
-angle2 = 100
-pwm2 = GPIO.PWM(26, 50)
-pwm2.start(8)
-pwm2.ChangeDutyCycle(angle2 / 18. + 3)
-time.sleep(0.3)
+    def Localize_Cam(self, angle=100):
+        pwm2 = GPIO.PWM(26, 50)
+        pwm2.start(8)
+        pwm2.ChangeDutyCycle(angle / 18. + 3)
+        time.sleep(0.3)
 
 def main():
-    for i in range(12):
-        GoAhead(100)
-        if (GPIO.input(15) == 0): # Infrared sensor changed!!
-            print('Detect No.'+str(i)+' plant!!!')
-            time.sleep(3000)
-            GoAhead(2000)
+    nav = Navi()
+    for i in range(99):
+        nav.Localize_Cam(angle=100-i)
+        print('I am here', i)
+    # for i in range(12):
+    #     nav.GoAhead(100)
+    #     if (GPIO.input(15) == 0): # Infrared sensor changed!!
+    #         print('Detect No.'+str(i)+' plant!!!')
+    #         time.sleep(3000)
+    #         nav.GoAhead(2000)
 
-    GPIO.cleanup()
-    sys.exit(0)
+    # GPIO.cleanup()
+    # sys.exit(0)
 
 if __name__ == "__main__":
     main()
